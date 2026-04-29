d59bd42acba3616e3a248669dd9ab8ab294e6c87
GTeses <2250033@tongji.edu.cn>
2026-04-29 15:57:17 +0800

    新增run_robot_only_closing_in脚本及其调式脚本v2,完成聚拢动作流程

---
+++ source/zhishu_dualarm_lab/tasks/robot_only/scene_cfg.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.sensors import FrameTransformerCfg
+from isaaclab.utils import configclass
+
+from zhishu_dualarm_lab.assets.robots.zhishu_robot import build_zhishu_robot_cfg
+from zhishu_dualarm_lab.tasks.dualarm_tabletop.cameras import (
+    build_external_camera_cfg,
+    build_left_wrist_camera_cfg,
+    build_right_wrist_camera_cfg,
+    build_waist_camera_cfg,
+)
+from zhishu_dualarm_lab.tasks.dualarm_tabletop.objects import build_ground_cfg, build_light_cfg
+from zhishu_dualarm_lab.utils.tcp_frames import make_tcp_frame
+
+from .constants import (
+    LEFT_TCP_LINK_NAME,
+    LEFT_TCP_NAME,
+    LEFT_TCP_OFFSET_POS,
+    LEFT_TCP_OFFSET_ROT,
+    RIGHT_TCP_LINK_NAME,
+    RIGHT_TCP_NAME,
+    RIGHT_TCP_OFFSET_POS,
+    RIGHT_TCP_OFFSET_ROT,
+)
+
+
+@configclass
+class ZhishuDualArmRobotOnlySceneCfg(InteractiveSceneCfg):
+    """Scene with only robot, light, ground, cameras, and TCP frames."""
+
+    ground = build_ground_cfg()
+    dome_light = build_light_cfg()
+    robot = build_zhishu_robot_cfg()
+    external_camera = build_external_camera_cfg()
+    waist_camera = build_waist_camera_cfg()
+    left_wrist_camera = build_left_wrist_camera_cfg()
+    right_wrist_camera = build_right_wrist_camera_cfg()
+    tcp_frames = FrameTransformerCfg(
+        prim_path="{ENV_REGEX_NS}/Robot/base_link",
+        debug_vis=False,
+        target_frames=[
+            make_tcp_frame(
+                prim_path=f"{{ENV_REGEX_NS}}/Robot/{LEFT_TCP_LINK_NAME}",
+                name=LEFT_TCP_NAME,
+                pos=LEFT_TCP_OFFSET_POS,
+                rot=LEFT_TCP_OFFSET_ROT,
+            ),
+            make_tcp_frame(
+                prim_path=f"{{ENV_REGEX_NS}}/Robot/{RIGHT_TCP_LINK_NAME}",
+                name=RIGHT_TCP_NAME,
+                pos=RIGHT_TCP_OFFSET_POS,
+                rot=RIGHT_TCP_OFFSET_ROT,
+            ),
+        ],
+    )
